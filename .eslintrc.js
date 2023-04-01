f8ea98a7f424ba5f3e18c3de2674f8a2200e313c
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-04-01 16:12:24 +0500

    add prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,13 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
+  rules: {},
+};
