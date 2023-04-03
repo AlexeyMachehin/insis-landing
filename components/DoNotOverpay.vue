d8c2b7ad62ec522d4a821589418664e451fb0fcb
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2023-04-03 16:53:29 +0500

    create DoNotOverpay adaptive

--- components/DoNotOverpay.vue
+++ components/DoNotOverpay.vue
@@ -3,7 +3,10 @@
     <div class="ladder-divider divider-rotate"></div>
     <div class="container">
       <div class="main-content">
-        <img src="@/static/images/wallet.svg" alt="wallet" />
+        <img
+          class="wallet-image"
+          src="@/static/images/wallet.svg"
+          alt="wallet" />
         <div class="description">
           <h2 class="do-not-overpay__title">
             <span class="accent-text accent-text-title"
@@ -11,7 +14,7 @@
             >
             За то, что другие навязывают.
           </h2>
-          <div>
+          <div class="description__content">
             <span class="accent-term">Рациональное потребление</span>
             – пользоваться тем, что действительно надо
           </div>
@@ -20,21 +23,31 @@
       <div class="advantages-list">
         <div class="advantage">
           <img
+            class="advantage__image"
             src="@/static/images/only-required-speed.svg"
             alt="only-required-speed" />
-          <div class="advantage-description">Только необходимая скорость</div>
+          <div class="advantage__description">
+            Только
+            <span class="advantage-accent-text">необходимая </span>скорость
+          </div>
         </div>
         <div class="advantage">
           <img
+            class="advantage__image"
             src="@/static/images/only-reasonable-price.svg"
             alt="only-reasonable-price" />
-          <div class="advantage-description">Только разумная цена</div>
+          <div class="advantage__description">
+            Только <span class="advantage-accent-text">разумная</span> цена
+          </div>
         </div>
         <div class="advantage">
           <img
+            class="advantage__image"
             src="@/static/images/only-favorable-conditions.svg"
             alt="only-favorable-conditions" />
-          <div class="advantage-description">Только выгодные условия</div>
+          <div class="advantage__description">
+            Только <span class="advantage-accent-text">выгодные</span> условия
+          </div>
         </div>
       </div>
     </div>
@@ -43,19 +56,31 @@
 </template>
 
 <style scoped>
+.advantage__image {
+  width: 86px;
+  height: 86px;
+}
+
+.advantage-accent-text {
+  font-weight: 600;
+  color: var(--reasonable-consumption-accent-text);
+}
+
 .accent-term {
   font-weight: 700;
 }
 
-.advantage-description {
+.advantage__description {
   width: 184px;
   margin-left: 35px;
-  margin-right: 120px;
   font-family: "Montserrat Alternates", sans-serif;
+  font-size: 22px;
+  line-height: 27px;
 }
 
 .advantages-list {
   display: flex;
+  justify-content: space-between;
   margin-top: 115px;
   margin-bottom: 160px;
   color: var(--font-var-2);
@@ -63,6 +88,7 @@
 
 .advantage {
   display: flex;
+  margin-right: 50px;
 }
 
 .do-not-overpay {
@@ -73,16 +99,74 @@
 .main-content {
   display: flex;
   align-items: center;
+  justify-content: space-between;
 }
 
 .description {
+  margin-left: 20px;
   max-width: 610px;
-  margin-left: 166px;
   color: var(--font-var-2);
 }
 
 .do-not-overpay__title {
-  /* font-weight: 700; */
   margin-bottom: 40px;
+}
+
+@media screen and (max-width: 1180px) {
+  .advantage {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .advantage__description {
+    margin-left: 0;
+    margin-top: 25px;
+  }
+}
+
+@media screen and (max-width: 1120px) {
+  .wallet-image {
+    width: 330px;
+    height: 350px;
+  }
+}
+
+@media screen and (max-width: 920px) {
+  .wallet-image {
+    width: 280px;
+    height: 300px;
+  }
+
+  .do-not-overpay__title {
+    font-size: 32px;
+  }
+
+  .description__content {
+    font-size: 20px;
+  }
+
+  .accent-term {
+    font-size: 20px;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  .wallet-image {
+    width: 123px;
+    height: 130px;
+  }
+
+  .advantage__description {
+    font-size: 16px;
+    width: 100px;
+  }
+
+  .advantage-accent-text {
+    font-size: 16px;
+  }
+
+  .main-content {
+    align-items: flex-start;
+  }
 }
 </style>
